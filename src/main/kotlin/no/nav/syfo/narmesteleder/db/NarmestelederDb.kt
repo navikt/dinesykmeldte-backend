525f7f805b94bab24b9b021a79892b608df06029
tiafiring <tia.firing@nav.no>
2022-02-28 13:10:59 +0100

    api for å deaktivere NL-kobling (#45)

--- src/main/kotlin/no/nav/syfo/narmesteleder/db/NarmestelederDb.kt
+++ src/main/kotlin/no/nav/syfo/narmesteleder/db/NarmestelederDb.kt
@@ -1,7 +1,9 @@
 package no.nav.syfo.narmesteleder.db
 
 import no.nav.syfo.application.database.DatabaseInterface
+import no.nav.syfo.application.database.toList
 import no.nav.syfo.narmesteleder.kafka.model.NarmestelederLeesahKafkaMessage
+import java.sql.ResultSet
 
 class NarmestelederDb(private val database: DatabaseInterface) {
     fun insertOrUpdate(narmesteleder: NarmestelederLeesahKafkaMessage) {
@@ -22,17 +24,40 @@ class NarmestelederDb(private val database: DatabaseInterface) {
         }
     }
 
-    fun remove(narmesteleder: NarmestelederLeesahKafkaMessage) {
+    fun remove(narmestelederId: String) {
         database.connection.use { connection ->
             connection.prepareStatement(
                 """
                delete from narmesteleder where narmeste_leder_id = ?;
             """
             ).use { ps ->
-                ps.setString(1, narmesteleder.narmesteLederId.toString())
+                ps.setString(1, narmestelederId)
                 ps.executeUpdate()
             }
             connection.commit()
         }
     }
+
+    fun finnNarmestelederkoblinger(narmesteLederFnr: String, orgnummer: String, fnrSykmeldt: String): List<NarmestelederDbModel> {
+        return database.connection.use { connection ->
+            connection.prepareStatement(
+                """
+           SELECT * from narmesteleder where leder_fnr = ? and orgnummer = ? and pasient_fnr = ?;
+        """
+            ).use {
+                it.setString(1, narmesteLederFnr)
+                it.setString(2, orgnummer)
+                it.setString(3, fnrSykmeldt)
+                it.executeQuery().toList { toNarmestelederDbModel() }
+            }
+        }
+    }
 }
+
+fun ResultSet.toNarmestelederDbModel(): NarmestelederDbModel =
+    NarmestelederDbModel(
+        narmestelederId = getString("narmeste_leder_id"),
+        pasientFnr = getString("pasient_fnr"),
+        lederFnr = getString("leder_fnr"),
+        orgnummer = getString("orgnummer")
+    )
