4574b3247d6e840279611162f719da445d921380
Andreas Nilsen <danduras@gmail.com>
2022-03-01 16:45:27 +0100

    try to connect to db

--- dinesykmeldte-kafka/src/main/kotlin/no/nav/syfo/Environment.kt
+++ dinesykmeldte-kafka/src/main/kotlin/no/nav/syfo/Environment.kt
@@ -1,5 +1,9 @@
 package no.nav.syfo
 
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.Paths
+
 data class Environment(
     val applicationPort: Int = getEnvVar("APPLICATION_PORT", "8080").toInt(),
     val applicationName: String = getEnvVar("NAIS_APP_NAME", "dinesykmeldte-kafka")
@@ -7,3 +11,5 @@ data class Environment(
 
 fun getEnvVar(varName: String, defaultValue: String? = null) =
     System.getenv(varName) ?: defaultValue ?: throw RuntimeException("Missing required variable \"$varName\"")
+
+fun getFileAsString(filePath: String) = String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8)
