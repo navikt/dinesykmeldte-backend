e607be8d59c118888de7ce719c979031846d696b
Andreas Nilsen <danduras@gmail.com>
2022-03-01 16:45:27 +0100

    try to connect to db

--- dinesykmeldte-kafka/src/main/kotlin/no/nav/syfo/Bootstrap.kt
+++ dinesykmeldte-kafka/src/main/kotlin/no/nav/syfo/Bootstrap.kt
@@ -1,13 +1,28 @@
 package no.nav.syfo
 
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.prometheus.client.hotspot.DefaultExports
 import no.nav.syfo.application.ApplicationServer
 import no.nav.syfo.application.ApplicationState
 import no.nav.syfo.application.createApplicationEngine
+import no.nav.syfo.database.GcpDatabase
+import no.nav.syfo.database.GcpDatabaseCredentials
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 val log: Logger = LoggerFactory.getLogger("no.nav.syfo.dinesykmeldte-kafka")
+val objectMapper: ObjectMapper = ObjectMapper().apply {
+    registerKotlinModule()
+    registerModule(JavaTimeModule())
+    configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+    configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+}
 
 fun main() {
     val env = Environment()
@@ -20,4 +35,6 @@ fun main() {
     val applicationServer = ApplicationServer(applicationEngine, applicationState)
     applicationServer.start()
     applicationState.ready = true
+    val credentials: GcpDatabaseCredentials = objectMapper.readValue(getFileAsString("/run/secrets/secret"))
+    val gcpDatabase = GcpDatabase(credentials, "dinesykmeldte-backend")
 }
