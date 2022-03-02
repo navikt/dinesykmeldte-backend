04c0e247314e3b733d1fce03364c6f77732eba45
Andreas Nilsen <danduras@gmail.com>
2022-03-02 13:31:48 +0100

    Gcp database (#49)
    * add privileges on tables in dinesykmeldte-backend db
    
    * Setup secret
    
    * try to connect to db
    
    * try to connect to db
    
    * add kafka

---
+++ dinesykmeldte-kafka/src/main/kotlin/no/nav/syfo/database/GcpDatabase.kt
@@ -0,0 +1,32 @@
+package no.nav.syfo.database
+
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import java.sql.Connection
+import java.util.Properties
+
+class GcpDatabase(credentials: GcpDatabaseCredentials, database: String) : DatabaseInterface {
+    private val dataSource: HikariDataSource
+    override val connection: Connection
+        get() = dataSource.connection
+
+    init {
+        val properties = Properties()
+        properties.setProperty("socketFactory", "com.google.cloud.sql.postgres.SocketFactory")
+        properties.setProperty("cloudSqlInstance", credentials.connectionName)
+        dataSource = HikariDataSource(
+            HikariConfig().apply {
+                dataSourceProperties = properties
+                jdbcUrl = "jdbc:postgresql:///$database"
+                username = credentials.username
+                password = credentials.password
+                maximumPoolSize = 1
+                minimumIdle = 1
+                isAutoCommit = false
+                connectionTimeout = 10_000
+                transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+                validate()
+            }
+        )
+    }
+}
---
+++ dinesykmeldte-kafka/src/main/kotlin/no/nav/syfo/database/GcpDatabaseCredentials.kt
@@ -0,0 +1,7 @@
+package no.nav.syfo.database
+
+data class GcpDatabaseCredentials(
+    val connectionName: String,
+    val password: String,
+    val username: String
+)
