14ff08483a81cd35c8c2f86efdda800f5098ea90
Tia Firing <tia.firing@nav.no>
2022-03-31 08:32:34 +0200

    fjerner kode som ikke brukes lenger

--- dinesykmeldte-backend/src/main/kotlin/no/nav/syfo/Environment.kt
+++ dinesykmeldte-backend/src/main/kotlin/no/nav/syfo/Environment.kt
@@ -11,30 +11,12 @@ data class Environment(
     val dbHost: String = getEnvVar("NAIS_DATABASE_HOST"),
     val dbPort: String = getEnvVar("NAIS_DATABASE_PORT"),
     val dbName: String = getEnvVar("NAIS_DATABASE_DATABASE"),
-    val narmestelederLeesahTopic: String = "teamsykmelding.syfo-narmesteleder-leesah",
-    val sendtSykmeldingTopic: String = "teamsykmelding.syfo-sendt-sykmelding",
-    val pdlScope: String = getEnvVar("PDL_SCOPE"),
-    val pdlGraphqlPath: String = getEnvVar("PDL_GRAPHQL_PATH"),
-    val aadAccessTokenUrl: String = getEnvVar("AZURE_OPENID_CONFIG_TOKEN_ENDPOINT"),
-    val clientId: String = getEnvVar("AZURE_APP_CLIENT_ID"),
-    val clientSecret: String = getEnvVar("AZURE_APP_CLIENT_SECRET"),
-    val syketilfelleEndpointURL: String = getEnvVar("SYKETILLFELLE_ENDPOINT_URL"),
-    val syketilfelleScope: String = getEnvVar("SYKETILLFELLE_SCOPE"),
     val cluster: String = getEnvVar("NAIS_CLUSTER_NAME"),
-    val sykepengesoknadTopic: String = "flex.sykepengesoknad",
-    val lestSykmeldingSoknadTopic: String = "teamsykmelding.dinesykmeldte-hendelser",
-    val hendelserTopic: String = "teamsykmelding.dinesykmeldte-hendelser-v2",
-    val runKafkaConsumer: Boolean = getRunKafkaProperty(),
     val nlResponseTopic: String = "teamsykmelding.syfo-narmesteleder"
 ) {
     fun jdbcUrl(): String {
         return "jdbc:postgresql://$dbHost:$dbPort/$dbName"
     }
-}
-
-private fun getRunKafkaProperty() = when (getEnvVar("RUN_KAFKA_CONSUMER", "true")) {
-    "false" -> false
-    else -> true
 }
 
 fun getEnvVar(varName: String, defaultValue: String? = null) =
