11cc7c9f1f00ace9ad59fd23e0f84ddcaf6f1dba
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2023-09-28 09:51:25 +0200

    Fixed https://github.com/advisories/GHSA-55g7-9cwv-5qfv with overriding transient from org.apache.kafka:kafka_2.12

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ val kotestVersion = "5.7.2"
 val googlePostgresVersion = "1.14.0"
 val googleOauthVersion = "1.34.1"
 val ktfmtVersion = "0.44"
-
+val snappyJavaVersion = "1.1.10.5"
 
 plugins {
     id("application")
@@ -63,6 +63,11 @@ repositories {
         implementation("io.ktor:ktor-serialization-jackson:$ktorVersion")
 
         implementation("no.nav.helse:syfosm-common-kafka:$smCommonVersion")
+        constraints {
+            implementation("org.xerial.snappy:snappy-java:$snappyJavaVersion") {
+                because("override transient from org.apache.kafka:kafka_2.12")
+            }
+        }
         implementation("no.nav.helse:syfosm-common-models:$smCommonVersion")
         implementation("no.nav.helse.flex:sykepengesoknad-kafka:$sykepengesoknadKafkaVersion")
 
