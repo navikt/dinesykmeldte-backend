2b9d74b1b4de39ff70ef14c59e4baee8698fcc34
Bjørn Faaberg <bjorn.faaberg@nav.no>
2025-10-20 10:30:36 +0200

    Fix route and us of pluging for isActiveSykmelding

--- src/main/kotlin/no/nav/syfo/sykmelding/api/SykmeldingApi.kt
+++ src/main/kotlin/no/nav/syfo/sykmelding/api/SykmeldingApi.kt
@@ -4,6 +4,7 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.post
+import io.ktor.server.routing.route
 import no.nav.syfo.dinesykmeldte.api.tryReceive
 import no.nav.syfo.sykmelding.SykmeldingService
 import no.nav.syfo.texas.TexasAzureADAuthPlugin
@@ -14,14 +15,17 @@ fun Route.registerSykmeldingApi(
     sykmeldingService: SykmeldingService, texasHttpClient: TexasHttpClient
 ) {
 
-    post("/api/sykmelding/isActiveSykmelding") {
+    route("/api/sykmelding/isActiveSykmelding") {
         install(TexasAzureADAuthPlugin) {
             this.client = texasHttpClient
         }
-        val request = call.tryReceive<IsActiveSykmeldingRequestDTO>()
-        securelog.info("Mottak kall mot /api/dinesykmeldte for pasientFnr: ${request.sykmeldtFnr}")
-        val result = sykmeldingService.getActiveSendtSykmeldingsperioder(request.sykmeldtFnr, orgnummer = request.orgnummer)
-        call.respond(HttpStatusCode.OK, result)
+        post {
+            val request = call.tryReceive<IsActiveSykmeldingRequestDTO>()
+            securelog.info("Mottak kall mot /api/dinesykmeldte for pasientFnr: ${request.sykmeldtFnr}")
+            val result =
+                sykmeldingService.getActiveSendtSykmeldingsperioder(request.sykmeldtFnr, orgnummer = request.orgnummer)
+            call.respond(HttpStatusCode.OK, result)
+        }
     }
 }
 
