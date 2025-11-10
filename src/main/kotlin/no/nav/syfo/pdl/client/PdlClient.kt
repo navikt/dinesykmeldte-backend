7aa5859b0284a1f59fe1b643d1b03e896d9fc687
Bjørn Faaberg <bjorn.faaberg@nav.no>
2025-11-10 13:59:03 +0100

    Add logging of status and message from PDL

--- src/main/kotlin/no/nav/syfo/pdl/client/PdlClient.kt
+++ src/main/kotlin/no/nav/syfo/pdl/client/PdlClient.kt
@@ -6,11 +6,11 @@ import io.ktor.client.request.header
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.http.HttpHeaders
-import io.ktor.http.cio.parseResponse
 import io.ktor.http.isSuccess
 import no.nav.syfo.pdl.client.model.GetPersonRequest
 import no.nav.syfo.pdl.client.model.GetPersonResponse
 import no.nav.syfo.pdl.client.model.GetPersonVariables
+import no.nav.syfo.util.logger
 import org.intellij.lang.annotations.Language
 
 @Language("GraphQL")
@@ -38,6 +38,9 @@ class PdlClient(
     private val httpClient: HttpClient,
     private val basePath: String,
 ) {
+    companion object {
+        private val log = logger()
+    }
     suspend fun getPerson(fnr: String, token: String): GetPersonResponse {
         val getPersonRequest =
             GetPersonRequest(
@@ -58,6 +61,7 @@ class PdlClient(
 
         } else {
             val responseText = response.body<String>()
+            log.error("Feil ved kall mot PDL: Status: ${response.status}. Message: $responseText")
             throw RuntimeException("Feil ved kall mot PDL: ${response.status}, $responseText")
         }
 
